145693f96d49edf54e29bef2059c5fc9c58cc03f
nief <shineniefei@qq.com>
2021-07-05 16:54:29 +0800

    new app

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication(exclude=
+        {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    @RequestMapping(value = "/")
+    public String index() {
+        log.info("Application index Response: Hello Spring Boot");
+        return "Hello Spring Boot";
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+        log.info("============= Start ON SpringBoot Success =============");
+    }
+
+}
